9349e42eef84c7076bed341d39195471495f681c
Fredrik Larsson <fredrik.larsson@nepartner.se>
2026-03-31 19:50:27 +0200

    feat: remove override to make husk overridable

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -6,6 +6,11 @@ HH.HuntersHusks = {
     Path = table.pack(...)[1]
 }
 
+if not HH.HuntersGeneticsBase then
+    error("ERROR: Hunter's Genetics Base mod is required for " .. HH.HuntersHusks.Name .. " to work! Get it from the Steam Workshop! Make sure it is loaded before " .. HH.HuntersHusks.Name .. "!");
+    return
+end
+
 dofile(HH.HuntersHusks.Path .. "/Lua/Scripts/shared.lua")
 
 if SERVER or not Game.IsMultiplayer then
